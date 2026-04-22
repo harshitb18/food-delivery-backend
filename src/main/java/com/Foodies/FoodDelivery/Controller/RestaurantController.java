3a3cfbe06aa3d7f7e806fe1474969ec47233872c
Harshith B <harshithb2002harshith@gmail.com>
2026-04-22 12:27:15 +0530

    user and restaurant controllers

---
+++ src/main/java/com/Foodies/FoodDelivery/Controller/RestaurantController.java
@@ -0,0 +1,40 @@
+package com.Foodies.FoodDelivery.Controller;
+
+import com.Foodies.FoodDelivery.Entity.Restaurant;
+import com.Foodies.FoodDelivery.Service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    @Autowired
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @PostMapping
+    public Restaurant saveRestaurant(@RequestBody Restaurant restaurant) {
+        return restaurantService.saveRestaurant(restaurant);
+    }
+
+    @GetMapping
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantService.getAllRestaurants();
+    }
+
+    @GetMapping("/{id}")
+    public Restaurant getRestaurantById(@PathVariable int id) {
+        return restaurantService.getRestaurantById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRestaurant(@PathVariable int id) {
+        restaurantService.deleteRestaurant(id);
+    }
+}
---
+++ src/main/java/com/Foodies/FoodDelivery/Controller/UserController.java
@@ -0,0 +1,40 @@
+package com.Foodies.FoodDelivery.Controller;
+
+import com.Foodies.FoodDelivery.Entity.User;
+import com.Foodies.FoodDelivery.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User saveUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+    }
+}
