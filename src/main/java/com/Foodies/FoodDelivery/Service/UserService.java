2e0230117f5df596924d19d3919f5a7d6ab2ceee
Harshith B <harshithb2002harshith@gmail.com>
2026-04-22 12:10:11 +0530

    user crud services

--- src/main/java/com/Foodies/FoodDelivery/Service/UserService.java
+++ src/main/java/com/Foodies/FoodDelivery/Service/UserService.java
@@ -1,14 +1,35 @@
 package com.Foodies.FoodDelivery.Service;
 
+import com.Foodies.FoodDelivery.Entity.User;
 import com.Foodies.FoodDelivery.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository=userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
     }
 }
